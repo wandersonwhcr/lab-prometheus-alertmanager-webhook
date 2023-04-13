93ff5ebcadb5fb9a5154a4ebff6438584b98ab59
Wanderson Camargo <wandersonwhcr@gmail.com>
2023-04-13 20:23:14 -0300

    Define a magic number

--- application/README.md
+++ application/README.md
@@ -2,4 +2,8 @@
 
 ```
 docker-compose run --rm application npm install
+
+docker-compose up --detach
+
+docker-compose exec redis redis-cli set application-magic-number 10
 ```
--- application/src/index.js
+++ application/src/index.js
@@ -1,3 +1,9 @@
+// Fastify -----------------------------------------------------------------------------------------
+
+const fastify = require("fastify")({
+  logger: true,
+});
+
 // Prometheus --------------------------------------------------------------------------------------
 const Prometheus = require("prom-client");
 
@@ -6,24 +12,28 @@ const register = new Prometheus.Registry();
 const magicNumberGauge = new Prometheus.Gauge({
   name: "my_project_application_magic_number",
   help: "My Project Application Magic Number",
+  collect: async function() {
+    const value = await fastify.redis.get("application-magic-number");
+    this.set(Number(value));
+  },
 });
 
 register.registerMetric(magicNumberGauge);
 
-// Fastify -----------------------------------------------------------------------------------------
+// Application -------------------------------------------------------------------------------------
 
-const fastify = require("fastify")({
-  logger: true,
+fastify.register(require("@fastify/redis"), {
+  url: process.env.REDIS_URL,
 });
 
-fastify.get('/', async function(request, reply) {
+fastify.get("/", async function(request, reply) {
   reply.status(200)
     .send({
       name: "application",
     });
 });
 
-fastify.get('/metrics', async function(request, reply) {
+fastify.get("/metrics", async function(request, reply) {
   const data = await register.metrics();
 
   reply.status(200)
